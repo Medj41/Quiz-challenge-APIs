5036940fe58b979049a358cb1255c7c76e2a31f0
Medj41 <larbi.djouama@gmail.com>
2024-01-15 22:19:46 +0000

    code test

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,4 +1,5 @@
-console.log('hello')
+
+
 
 let startButton = document.querySelector('#start');
 let questions = document.querySelector('#questions');
@@ -7,19 +8,55 @@ let choices = document.querySelector('#choices')
 let startScreen = document.querySelector('#start-screen')
 let buttonOption = '';
 
+let Questions = [
+  {
+    question: 'Pick a number',
+    choises: ['1', '2', '3','4'],
+    answer: 2
+  },
+  {
+    question: 'Pick a number',
+    choices: ['A', 'B', 'C', 'D'],
+    Answer: "'c"
+  },
+  {
+    question: 'pick a symbol',
+    choices: ['£', '$', '%', '&'],
+    correctAnswer: "£"
+  },
+  {
+    question: 'pick a team',
+    choices: ['Real', 'Barca', 'juve', 'Milan'],
+    correctAnswer: "'Real'"
+  }
+]
 
 let questionOne = 'Pick a number';
 let optionsOne = [1, 2, 3, 4];
 
+
 let questionTwo = 'Pick a letter';
-let optionsTwo = ['A','B','C','D'];
+let optionsTwo = ['A', 'B', 'C', 'D'];
 
 let questionThree = 'pick a symbol';
-let optionsThree = ['£','$','%','&'];
+let optionsThree = ['£', '$', '%', '&'];
+
 
 let questionsFour = 'pick a team'
-let optionsFour = ['Real','Barca','juve','Milan'];
+let optionsFour = ['Real', 'Barca', 'juve', 'Milan'];
 
+
+// for (const key in object) {
+//   if (Object.hasOwnProperty.call(object, key)) {
+//     const element = object[key];
+
+//   }
+// }
+
+
+
+
+console.log(Questions[0].question);
 // var listEl = document.createElement("ol");
 
 // var li1 = document.createElement("li");
@@ -48,36 +85,65 @@ let optionsFour = ['Real','Barca','juve','Milan'];
 startButton.addEventListener("click", function (event) {
   startScreen.setAttribute("style", "display: none;");
   questions.setAttribute("style", "display: contents;");
-  
-  creatingLi(questionOne, optionsOne );
+  creatingLi(questionOne, optionsOne);
+
+
 
   event.stopPropagation();
 
 });
 
 
+var optionChosen = document.querySelector('#choices')
 
 
-function creatingLi( question , options) {
-  questionsTitle.innerHTML = question ;
-  var ol = document.createElement('ol');
-  for (i = 0; i < options.length; i++) {
+optionChosen.addEventListener('click', function (event) {
+  let buttonClicked = event.target.innerHTML
+  console.log(event.target.innerHTML);
+  let scoreForEach = document.createElement('p');
+  choices.appendChild(scoreForEach);
+  if (buttonClicked == 2) {
+    scoreForEach.innerHTML = 'Correct!'
+    console.log('correct');
+  } else {
+    return scoreForEach.innerHTML = 'Wrong!'
+  }
+  console.log(event);
+
+})
+
+
+function creatingLi() {
+  let displayedQuestion = '';
+  let displayedchoices = '';
+  for (let i = 0; i < Questions.length; i++) {
+    displayedQuestion = Questions[i];
+    displayedchoices = Questions[i]
+
+    questionsTitle.innerHTML = displayedQuestion.question;
+
+
+
+    var ol = document.createElement('ol');
+    // for (i = 0; i < choices.length; i++) {
+    //   choices = Questions.choices[i]
+
+
     var button = document.createElement('button');
-    var li = document.createElement('li');
-    li.innerHTML = options[i];
+    button.setAttribute('id', 'optionss')
     ol.appendChild(button);
-    button.appendChild(li);
-    button.innerHTML = li.innerHTML;
-    console.log(li);
+    button.innerHTML = displayedchoices.choices
+    console.log(button.innerHTML);
   }
   document.getElementById('choices').appendChild(ol);
+
+
 }
 
-function pickCorrectAnswer(){
 
 
-};
 
 
-console.log(li.value);
+
+console.log(document.body);
 
